4cb3d9f37c18b087622e26170dcc7732e3d74e30
Diki Center <dikimistak@gmail.com>
2023-02-16 16:43:33 +0700

    Update : Menuju Final Project

--- relationdb_java/src/main/java/com/example/relationdb_java/Controller/AnggotaController.java
+++ relationdb_java/src/main/java/com/example/relationdb_java/Controller/AnggotaController.java
@@ -1,12 +1,14 @@
 package com.example.relationdb_java.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.relationdb_java.Entity.Anggota;
@@ -45,9 +47,31 @@ public class AnggotaController {
     }
 
     // Halaman Edit Data Anggota
-    @GetMapping("/edit_anggota")
-    public String editAnggota() {
+    @GetMapping("/edit_anggota/{id_anggota}")
+    public String editAnggota(@PathVariable(value = "id_anggota") Integer id_anggota, Model model) {
+
+        Optional<Anggota> OptAnggota = AnggotaRepo.findById(id_anggota);
+        Anggota anggota = OptAnggota.get();
+        model.addAttribute("dataAnggota", anggota);
+
         return "Contents/Anggota/EditAnggota";
+    }
+
+    @PostMapping("/update_anggota")
+    public String updateAnggota(@ModelAttribute Anggota anggota, HttpSession session) {
+
+        AnggotaRepo.save(anggota);
+
+        return "redirect:/anggota";
+    }
+
+    // Hapus Data
+    @GetMapping("/delete_anggota/{id_anggota}")
+    public String deleteAnggota(@PathVariable(value = "id_anggota") Integer id_anggota, HttpSession session) {
+
+        AnggotaRepo.deleteById(id_anggota);
+
+        return "redirect:/anggota";
     }
 
 }
--- relationdb_java/src/main/java/com/example/relationdb_java/Controller/BukuController.java
+++ relationdb_java/src/main/java/com/example/relationdb_java/Controller/BukuController.java
@@ -1,12 +1,14 @@
 package com.example.relationdb_java.Controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.relationdb_java.Entity.Buku;
@@ -45,9 +47,31 @@ public class BukuController {
     }
 
     // Halaman Edit Buku
-    @GetMapping("/edit_buku")
-    public String editBuku() {
+    @GetMapping("/edit_buku/{id_buku}")
+    public String editBuku(@PathVariable(value = "id_buku") Integer id_buku, Model model) {
+
+        Optional<Buku> OptBuku = BukuRepo.findById(id_buku);
+        Buku buku = OptBuku.get();
+        model.addAttribute("dataBuku", buku);
+
         return "Contents/Buku/EditBuku";
+    }
+
+    @PostMapping("/update_buku")
+    public String updateBuku(@ModelAttribute Buku buku, HttpSession session) {
+
+        BukuRepo.save(buku);
+
+        return "redirect:/buku";
+    }
+
+    // Hapus Data
+    @GetMapping("/delete_buku/{id_buku}")
+    public String deleteBuku(@PathVariable(value = "id_buku") Integer id_buku, HttpSession session) {
+
+        BukuRepo.deleteById(id_buku);
+
+        return "redirect:/buku";
     }
 
 }
